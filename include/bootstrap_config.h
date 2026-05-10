4cdd5aef613af17fe57e312dff22c02265fba48f
Devin P Blagbrough <dblagbro@gmail.com>
2026-05-10 01:02:19 -0400

    Move bootstrap OTA to central firmware URLs

--- include/bootstrap_config.h
+++ include/bootstrap_config.h
@@ -4,9 +4,14 @@ namespace BootstrapConfig {
   static constexpr char WIFI_SSID[] = "SpectrumSetup-4D";
   static constexpr char WIFI_PASSWORD[] = "smalltruck536";
 
-  static constexpr char FIRMWARE_BASE_URL[] = "https://www.voipguru.org/";
-  static constexpr char FIRMWARE_FILENAME[] = "rebooter-firmware-main.bin";
-  static constexpr char CURRENT_VERSION[] = "bootstrap-0.1.0";
+  static constexpr char PRIMARY_FIRMWARE_URL[] = "https://www.voipguru.org/rebooter/firmware/stable/latest.bin";
+  static constexpr char SECONDARY_FIRMWARE_URL[] = "https://www2.voipguru.org/rebooter/firmware/stable/latest.bin";
+  static constexpr const char* FIRMWARE_URLS[] = {
+    PRIMARY_FIRMWARE_URL,
+    SECONDARY_FIRMWARE_URL
+  };
+  static constexpr size_t FIRMWARE_URL_COUNT = sizeof(FIRMWARE_URLS) / sizeof(FIRMWARE_URLS[0]);
+  static constexpr char CURRENT_VERSION[] = "bootstrap-0.2.0";
 
   static constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 30000;
   static constexpr uint32_t WIFI_RETRY_DELAY_MS = 10000;
